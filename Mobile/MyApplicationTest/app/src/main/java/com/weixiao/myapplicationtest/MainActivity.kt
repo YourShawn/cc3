1e4c3af79eb46901801b5c77b1263f866f89b777
Shawn <your.xiaowei@gmail.com>
2023-06-01 15:51:15 -0400

    complete the coding

--- Mobile/MyApplicationTest/app/src/main/java/com/weixiao/myapplicationtest/MainActivity.kt
+++ Mobile/MyApplicationTest/app/src/main/java/com/weixiao/myapplicationtest/MainActivity.kt
@@ -2,13 +2,37 @@ package com.weixiao.myapplicationtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.FirebaseDatabase
+import com.weixiao.myapplicationtest.adapters.PeopleAdapter
+import com.weixiao.week1class.entity.People
 
 /**
  * This is main page.
  */
 class MainActivity : AppCompatActivity() {
+    private var adapter: PeopleAdapter? = null;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
+        //Get instance and its child
+        val query = FirebaseDatabase.getInstance().reference.child("people");
+        //Get the Firebase recycle option instance
+        val option = FirebaseRecyclerOptions.Builder<People>().setQuery(query,People::class.java).build();
+        //Pass the Firebase recycle option instance to PeopleAdaptor constructor than get the instance of adapter.
+        adapter = PeopleAdapter(option);
+
+        val rView : RecyclerView = findViewById(R.id.rView);
+        rView.layoutManager = LinearLayoutManager(this);
+        rView.adapter = adapter;
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Call
+        adapter?.startListening();
     }
 }
--- Mobile/MyApplicationTest/app/src/main/java/com/weixiao/myapplicationtest/adapters/PeopleAdapter.kt
+++ Mobile/MyApplicationTest/app/src/main/java/com/weixiao/myapplicationtest/adapters/PeopleAdapter.kt
@@ -1,4 +1,12 @@
-package com.weixiao.week1class.adapters
+package com.weixiao.myapplicationtest.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.weixiao.myapplicationtest.R
+import com.weixiao.week1class.entity.People
 
 
 /**
@@ -6,5 +14,24 @@ package com.weixiao.week1class.adapters
  * But it is empty now.
  */
 
-class PeopleAdapter {
+class PeopleAdapter(option: FirebaseRecyclerOptions<People>) :
+    FirebaseRecyclerAdapter<People, PeopleAdapter.MyViewHolder>(option) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return MyViewHolder(inflater, parent);
+    }
+
+    /**
+     * Must override this method from the super class
+     */
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int, model: People) {
+
+    }
+
+    class MyViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
+        RecyclerView.ViewHolder(inflater.inflate(R.layout.row_layout, parent, false)) {
+
+    }
+
 }
